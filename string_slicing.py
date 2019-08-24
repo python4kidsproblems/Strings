d23471a9166c9ddff5e31772c8ce7d5bdd2f0a70
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 08:21:49 +0500

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,39 @@
+# String Slicing Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program that replaces the first letter of a word with the last.#
+# for example: "Hello" -> "oellH"                                        #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a program that replaces the last letter of a string with ing     #
+# e.g "hope" -> hoping, "apple" -> "appling", "run" -> "ruing"           #
+# Don't worry this is just for fun, the words don't have to make sense.  #
+##########################################################################
+
+# your code goes here
+
+################################### Q3 ###################################
+# Write a program that takes two words and joins the first three letter  #
+# of the first word with the last three letter of the second word.       #
+# e.g "hello" "world" -> "helrld"                                        #
+# now make it so it adds a dash in the middle of the two:                #
+# e.g "hello" "world" -> "hel-rld"                                       #
+##########################################################################
+
+# your code goes here
+
+################################### Q4 ###################################
+# Write a program that gets the middle letter of a word.                 #
+# if the number has an even number of letters, then get either of the    #
+# middle letters.                                                        #
+# e.g -> "hello" -> "l", "a", "cake" -> "a" or "k" (both are fine)       #
+# Remember the len() function? You may have to use that.                 #
+##########################################################################
+
+# your code goes here
