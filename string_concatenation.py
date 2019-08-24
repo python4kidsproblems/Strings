aa4b1a1014b1d6a85c1b8e1cdcc3af2da21169c9
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 07:56:16 +0500

    string concatenation

---
+++ string_concatenation.py
@@ -0,0 +1,24 @@
+################################### Q1 ###################################
+# Write a program that takes in user's fathers and mothers full names,   #
+# and prints by filling in the blanks:                                   #
+# "Oh so your father's name is ___ and your mother's name is ___"        #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a program that asks the user to enter their name, their age and  # 
+# their school and prints by filling in the blanks:                      #
+# "Hello, I am ___. I am ___ years old, and I go to ___ school."         #
+##########################################################################
+
+# your code goes here
+
+################################### Q3 ###################################
+# Wrire a program that acts like a parrot and repeats everything the user#
+# writes.                                                                #
+# Take input from the user, and as soon as the input is entered, print   #
+# "hmmm, so you just said ___", by filling the blank with the user input.#
+##########################################################################
+
+# your code goes here
