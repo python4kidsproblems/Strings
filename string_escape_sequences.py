e1b092d980b32262987f1767482e280f66239d29
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 08:43:07 +0500

    string escape sequences

---
+++ string_escape_sequences.py
@@ -0,0 +1,38 @@
+# Strings Escape Sequences Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program prints this correctly:                                 #
+#                                                                        #
+# Hello World, how's it going?                                           #
+# 	I hope all is well                                                   # 
+# Let's try printing a backslash: /                                      #
+# Cool! We can try printing quotes now: " '                              #
+# This is fun!                                                           #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a program that prints this paragraph correctly, using escape     #
+# sequences (look for tabs, newlines etc.):                              #
+# '''                                                                    #
+# 	Alice was beginning to get very tired of sitting by her sister on    #
+# the bank, and of having nothing to do: once or twice she had peeped    #
+# into the book her sister was reading, but it had no pictures or        #
+# conversations in it, `and what is the use of a book,' thought Alice    #
+# `without pictures or conversation?'                                    #
+#
+# 	So she was considering in her own mind (as well as she could, for    #
+# the hot day made her feel very sleepy and stupid), whether the         #
+# pleasure of making a daisy-chain would be worth the trouble of getting #
+# up and picking the daisies, when suddenly a White Rabbit with pink     #
+# eyes ran close by her.                                                 #
+#'''                                                                     #
+#                                                                        #
+# Copy paste the paragraph to make it easier.                            #
+##########################################################################
+
+# your code goes here
